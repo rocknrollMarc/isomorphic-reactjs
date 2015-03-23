138d16de985db7710ac733880a215099c98f4f57
Marc Bluemner <marc.bluemner@gmail.com>
2015-03-23 21:49:56 +0100

    postHeader jsx component

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -10,3 +10,16 @@ var Post = React.createClass({
     );
   }
 });
+
+var PostHeader = React.createClass({
+  render: function() {
+    return (
+      <div className="post-header">
+        <h2>{this.props.post.title}</h2>
+        <div className="post-meta">
+          By {this.props.post.author} - {this.props.post.created_at}
+        </div>
+      </div>
+    );
+  }
+});
