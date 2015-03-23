9d92fede33fda8c7e315e4ab5dd0818a639a3e02
Marc Bluemner <marc.bluemner@gmail.com>
2015-03-23 21:59:02 +0100

    postscontent jsx component

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -23,3 +23,13 @@ var PostHeader = React.createClass({
     );
   }
 });
+
+var PostContent = React.createClass({
+  render: function() {
+    return (
+      <div className="post-contents">
+        {this.props.post.contents}
+      </div>
+    );
+  }
+});
